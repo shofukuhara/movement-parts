5407f5ead431b93360f56b4787668f70cf1fa1cf
shofukuhara <sho.fukuhara0213@gmail.com>
2022-09-11 22:33:26 +0900

    mouseStalker(拡大)

--- mouseStalker/src/js/main.js
+++ mouseStalker/src/js/main.js
@@ -1,6 +1,20 @@
 const stalker = document.getElementById("stalker");
+let hover = false;
+const btn = document.querySelector(".btn");
 
 document.addEventListener("mousemove", (e) => {
-  stalker.style.transform =
-    "translate(" + e.clientX + "px," + e.clientY + "px)";
+  if (!hover) {
+    stalker.style.transform =
+      "translate(" + e.clientX + "px," + e.clientY + "px)";
+  }
+});
+
+btn.addEventListener("mouseover", () => {
+  hover = true;
+  stalker.classList.add("hov_");
+});
+
+btn.addEventListener("mouseout", () => {
+  hover = false;
+  stalker.classList.remove("hov_");
 });
