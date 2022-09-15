8983b184728904893a76738e57a6fe14b00babe4
shofukuhara <sho.fukuhara0213@gmail.com>
2022-09-15 21:16:20 +0900

    ScreenTransition

--- ScreenTransition/src/js/main.js
+++ ScreenTransition/src/js/main.js
@@ -1,9 +1,11 @@
 const logo = document.querySelector(".splash__logo");
-const splash = document.getElementById("splash");
+const splash = document.querySelector(".splash");
 
 window.addEventListener("load", () => {
   setTimeout(() => {
     logo.classList.add("fade");
-    splash.classList.add("splash");
   }, 1000);
+  setTimeout(() => {
+    splash.classList.add("splash-bg");
+  }, 1500);
 });
