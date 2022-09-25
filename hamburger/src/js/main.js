10a2e45bac00b96320f00bdc56447989ee66ec8f
shofukuhara <sho.fukuhara0213@gmail.com>
2022-09-25 23:03:51 +0900

    add:nav追加

--- hamburger/src/js/main.js
+++ hamburger/src/js/main.js
@@ -1,9 +1,11 @@
 const open = document.querySelector(".header__hamburger");
 const menu = document.querySelector(".header__menu");
 const close = document.querySelector(".header__menu-hamburger");
+const nav = document.querySelector(".header__menu-nav");
 
 open.addEventListener("click", () => {
   menu.classList.add("display");
+  nav.classList.add("display");
   setTimeout(() => {
     close.classList.toggle("display");
   }, 500);
@@ -13,5 +15,6 @@ close.addEventListener("click", () => {
   close.classList.remove("display");
   setTimeout(() => {
     menu.classList.remove("display");
+    nav.classList.remove("display");
   }, 500);
 });
